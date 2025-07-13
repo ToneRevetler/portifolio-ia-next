a6daf8100fe932787073bb4bfdf58f2fb3b69b57
Tone <mirandatone@outlook.com>
2025-07-13 20:40:25 -0300

    feat: add `InfinityScroll` component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import Initial from "../../components/Hero"
 import ProjectSection from "../../components/ProjectSection"
 import AboutMe from "../../components/AboutMe"
 import ButtonBackToTop from "../../components/ButtonBackToTop"
+import { InfinityScroll } from "@/../components/InfinityScroll"
 
 export default function Home() {
   return (<>
     <Initial />
     < ProjectSection />
+    < InfinityScroll />
     <AboutMe />
     <ButtonBackToTop />
   </>
