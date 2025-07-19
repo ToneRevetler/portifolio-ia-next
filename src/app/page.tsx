0b765759533a48554637a2a8f8f65c19d092fdaf
Tone <mirandatone@outlook.com>
2025-07-19 20:25:04 -0300

    fix: infinity scroll visible

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Initial from "../../components/Hero"
 import ProjectSection from "../../components/ProjectSection"
 import AboutMe from "../../components/AboutMe"
 import ButtonBackToTop from "../../components/ButtonBackToTop"
-import { InfinityScroll } from "@/../components/InfinityScroll"
+import { InfinityScroll } from "../../components/InfinityScroll/InfinityScroll"
+import ExperienceAccordion from "@/../components/Accordion/Accordion"
 
 export default function Home() {
   return (<>
     <Initial />
     < ProjectSection />
+    < ExperienceAccordion />
     < InfinityScroll />
     <AboutMe />
     <ButtonBackToTop />
