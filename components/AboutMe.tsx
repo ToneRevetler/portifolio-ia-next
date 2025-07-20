adf9b81fa1aaa1f1162047a8d392ce138b3c9d97
Tone <mirandatone@outlook.com>
2025-07-19 21:14:38 -0300

    refactor: remove unused SpinningText component and related imports

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -1,8 +1,6 @@
 import { Lexend } from "next/font/google";
 import Image from "next/image";
 import SocialMedia from "../components/social/SocialMedia";
-import { CiLocationArrow1 } from "react-icons/ci";
-import { SpinningTextBasic } from '@/assets/gps-lib/SpinningText';
 
 const titleFont = Lexend({ subsets: ["latin"], weight: "700" });
 
@@ -20,12 +18,6 @@ export default function AboutMe() {
           < SocialMedia />
         </div>
         <div  className="relative">
-          <a href="https://api.whatsapp.com/send/?phone=5519981135074&text=Vi%20seu%20portf%C3%B3lio%20e%20estou%20entrando%20em%20contato%21&type=phone_number&app_absent=0" className=" cursor-pointer absolute bottom-8 right-8 bg-white h-24 w-24 rounded-full flex items-center justify-center shadow-lg text-gray-600">
-            < SpinningTextBasic />
-            <div className="absolute h-12 w-12 border-gray-300 border solid rounded-full flex justify-center items-center" >
-              <CiLocationArrow1 className="text-xl text-gray-600" />
-            </div>
-          </a>
           <Image src="/images/about/me.png" alt="foto minha" width={500} height={192} className="w-full object-cover object-center rounded-md" />
         </div>
 
